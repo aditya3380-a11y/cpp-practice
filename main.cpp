a9b584bbf03a188229854678df06eb795b475d06
Aditya Pandey <officialaditya3380@gmail.com>
2026-08-23 22:44:43 +0530

    Add first C++ program: sum of two numbers

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std;
+
+int main() {
+    // Understanding variables and printing sum of two numbers in our program
+    int firstNumber= 10;
+    int secondnumber= 20;
+    int sum = firstNumber + secondnumber;
+
+    cout <<"sum of two number are="<<sum;
+    return 0;
+}
