0f03c7593b555dd24a4a3a1242151bb068d7b20f
Aditya Pandey <officialaditya3380@gmail.com>
2026-08-23 23:57:16 +0530

    Add operators and data type conversion practice

---
+++ datatypes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int n1= 1;
+    int n2 = 2;
+    double n3=(double) n1/n2;
+    //  explicit type conversion 
+
+
+
+    cout << "final="<<  n3 << "\n" ;
+
+    return 0;
+}
---
+++ operators.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a=20;
+    cout << (a += 5) << "\n";
+    cout << (a *= 4) << "\n";
+    cout << (a /= 2) << "\n";
+    cout << (a -= 20) << "\n";
+
+
+
+    return 0;
+}
