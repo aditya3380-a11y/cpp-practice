d0d0dd956a200c4271aed9fb77c280e7b05f77cf
Aditya Pandey <officialaditya3380@gmail.com>
2026-08-27 21:58:41 +0530

    cpp program of area and perimeter

---
+++ area.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float l, b ;
+    cout << "Hello! welcome to rectangle area solver and perimeter solver.Please drop your value of length and breadth=";
+    cin >> l >> b ;
+
+    float area , perimeter;
+    area= (l*b);
+    perimeter = (2*(l+b));
+    cout << " area ="<< area << " and perimeter is " << perimeter << "\n";
+    return 0;
+}
