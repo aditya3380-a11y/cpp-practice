1b4f469e574d5b6d990b3367db797f0a0e7d76e5
Aditya Pandey <officialaditya3380@gmail.com>
2026-08-28 21:39:28 +0530

    Loop problems

---
+++ forloop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Please input any number = ";
+    cin >> num;
+
+    for ( int i = 1; i <= num ; i++)
+    {
+        cout << "Hello world" << "\n";
+    }
+    
+    return 0;
+}
